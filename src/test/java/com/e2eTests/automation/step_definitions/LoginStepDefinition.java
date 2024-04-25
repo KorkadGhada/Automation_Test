6e60e278b32f10042b51398130677643fa718a5d
ghada_korkad <korkad.ghada@gmail.com>
2024-04-25 22:40:45 +0200

    update project

--- src/test/java/com/e2eTests/automation/step_definitions/LoginStepDefinition.java
+++ src/test/java/com/e2eTests/automation/step_definitions/LoginStepDefinition.java
@@ -2,6 +2,7 @@ package com.e2eTests.automation.step_definitions;
 
 import com.e2eTests.automation.page_objects.LoginPage;
 import com.e2eTests.automation.utils.ConfigFileReader;
+import com.e2eTests.automation.utils.SeleniumUtils;
 import com.e2eTests.automation.utils.Setup;
 
 import io.cucumber.java.en.Given;
@@ -11,10 +12,12 @@ import io.cucumber.java.en.When;
 public class LoginStepDefinition {
 	public LoginPage loginPage;
 	public ConfigFileReader configFileReader;
+	public SeleniumUtils seleniumUtils;
 
 	public LoginStepDefinition() {
 		loginPage = new LoginPage();
 		configFileReader = new ConfigFileReader ();
+		seleniumUtils = new SeleniumUtils();
 	}
 	
 
@@ -22,25 +25,34 @@ public class LoginStepDefinition {
 	@Given("Je visite l'application NopCommerce")
 	public void jeVisiteLApplicationNopCommerce() {
 		//Setup.getDriver().get("https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F");
-		Setup.getDriver().get(configFileReader.getProperties("home.url"));
+		//Setup.getDriver().get(configFileReader.getProperties("home.url"));
+		seleniumUtils.get(configFileReader.getProperties("home.url"));
 	}
 
 	@When("Je saisis l'adresse mail {string}")
 	public void jeSaisisLAdresseMail(String email) {
+		//LoginPage.getEmail().sendKeys(configFileReader.getProperties("home.login"));
+		/*
 		LoginPage.getEmail().clear();
 		LoginPage.getEmail().sendKeys(email);
-		//LoginPage.getEmail().sendKeys(configFileReader.getProperties("home.login"));
+		*/
+		
+		seleniumUtils.writeText(LoginPage.getEmail(), email);
 	}
 
 	@When("Je saisis le login {string}")
 	public void jeSaisisLeLogin(String login) {
+		/*
 		LoginPage.getPassword().clear();
 		LoginPage.getPassword().sendKeys(login);
+		*/
+		seleniumUtils.writeText(LoginPage.getPassword(), login);
 	}
 
 	@When("Je clique sur le bouton Login")
 	public void jeCliqueSurLeBoutonLogin() {
-		LoginPage.getBtnLogin().click();
+		//LoginPage.getBtnLogin().click();
+		seleniumUtils.click(LoginPage.getBtnLogin());
 	}
 
 	@Then("Je me redirige vers la page home")
@@ -50,6 +62,11 @@ public class LoginStepDefinition {
 	/* login invalid */
 	@Then("Je verifie le message d'erreur")
 	public void jeVerifieLeMessageDErreur() {
+	}
+	
+	/*Outline*/
+	@Then("Je verifie le message {string}")
+	public void jeVerifieLeMessage(String message) {
 	}
 
 
--- src/test/java/com/e2eTests/automation/utils/BasePage.java
+++ src/test/java/com/e2eTests/automation/utils/BasePage.java
@@ -1,14 +1,21 @@
 package com.e2eTests.automation.utils;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class BasePage {
-protected WebDriver driver;
-	
+	protected WebDriver driver;
+	/** log. */
+	protected static Logger log = LogManager.getLogger();
+
 	public BasePage(WebDriver driver) {
 		
+
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
-		
+
 	}
 
 }
